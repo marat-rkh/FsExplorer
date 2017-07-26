5b836a92bf03cd806b90aefcc4ae4f94e76e6405
Marat Habibullin <maratx387@gmail.com>
2017-07-26 20:50:04 +0300

    Basic panes are created

--- src/main/java/fs/explorer/gui/MainWindow.java
+++ src/main/java/fs/explorer/gui/MainWindow.java
@@ -5,28 +5,39 @@ import java.awt.*;
 
 public final class MainWindow {
     private final String name;
+    private final int WINDOW_WIDTH = 640;
+    private final int WINDOW_HEIGHT = 480;
 
     public MainWindow(String name) {
         this.name = name;
     }
 
     public void show() {
-        javax.swing.SwingUtilities.invokeLater(this::createAndShow);
+        SwingUtilities.invokeLater(this::createAndShow);
     }
 
     private void createAndShow() {
         JFrame frame = new JFrame(name);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-        JLabel yellowLabel = new JLabel();
-        yellowLabel.setOpaque(true);
-        yellowLabel.setBackground(new Color(248, 213, 131));
-        yellowLabel.setPreferredSize(new Dimension(200, 180));
+        frame.getContentPane().setPreferredSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
 
         frame.setJMenuBar(MenuBar.create());
-        frame.getContentPane().add(yellowLabel, BorderLayout.CENTER);
+        frame.getContentPane().add(createSplitPane(), BorderLayout.CENTER);
 
         frame.pack();
         frame.setVisible(true);
+    }
+
+    private JSplitPane createSplitPane() {
+        JSplitPane treeAndPreviewPanes = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
+                new JLabel(), new JLabel());
+        treeAndPreviewPanes.setOneTouchExpandable(true);
+        treeAndPreviewPanes.setDividerLocation(WINDOW_WIDTH / 3);
+
+        JSplitPane mainAndStatusPanes = new JSplitPane(JSplitPane.VERTICAL_SPLIT,
+                treeAndPreviewPanes, new JLabel("Status: OK"));
+        mainAndStatusPanes.setOneTouchExpandable(true);
+        mainAndStatusPanes.setDividerLocation(9 * WINDOW_HEIGHT / 10);
+        return mainAndStatusPanes;
     }
 }
