e380705ae0cf4e0046a6e2f1ec09ca7830af3477
Marat Habibullin <maratx387@gmail.com>
2017-08-02 20:50:18 +0300

    AsyncPreivewProvider handles task rejections now

--- src/main/java/fs/explorer/providers/preview/AsyncPreviewProvider.java
+++ src/main/java/fs/explorer/providers/preview/AsyncPreviewProvider.java
@@ -5,12 +5,15 @@ import fs.explorer.providers.dirtree.TreeNodeData;
 import javax.swing.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
 public class AsyncPreviewProvider implements PreviewProvider {
     private final PreviewProvider previewProvider;
     private final ExecutorService executor = Executors.newSingleThreadExecutor();
+
+    private static final String INTERNAL_ERROR = "internal error";
 
     public AsyncPreviewProvider(PreviewProvider previewProvider) {
         this.previewProvider = previewProvider;
@@ -22,14 +25,17 @@ public class AsyncPreviewProvider implements PreviewProvider {
             Consumer<JComponent> onComplete,
             Consumer<String> onFail
     ) {
-        // TODO handle RejectedExecutionException
-        executor.execute(() ->
-            previewProvider.getTextPreview(
-                data,
-                arg -> SwingUtilities.invokeLater(() -> onComplete.accept(arg)),
-                arg -> SwingUtilities.invokeLater(() -> onFail.accept(arg))
-            )
-        );
+        try {
+            executor.execute(() ->
+                previewProvider.getTextPreview(
+                    data,
+                    arg -> SwingUtilities.invokeLater(() -> onComplete.accept(arg)),
+                    arg -> SwingUtilities.invokeLater(() -> onFail.accept(arg))
+                )
+            );
+        } catch (RejectedExecutionException e) {
+            onFail.accept(INTERNAL_ERROR);
+        }
     }
 
     @Override
@@ -38,14 +44,17 @@ public class AsyncPreviewProvider implements PreviewProvider {
             Consumer<JComponent> onComplete,
             Consumer<String> onFail
     ) {
-        // TODO handle RejectedExecutionException
-        executor.execute(() ->
-            previewProvider.getImagePreview(
-                data,
-                arg -> SwingUtilities.invokeLater(() -> onComplete.accept(arg)),
-                arg -> SwingUtilities.invokeLater(() -> onFail.accept(arg))
-            )
-        );
+        try {
+            executor.execute(() ->
+                previewProvider.getImagePreview(
+                    data,
+                    arg -> SwingUtilities.invokeLater(() -> onComplete.accept(arg)),
+                    arg -> SwingUtilities.invokeLater(() -> onFail.accept(arg))
+                )
+            );
+        } catch (RejectedExecutionException e) {
+            onFail.accept(INTERNAL_ERROR);
+        }
     }
 
     // TODO add to resource cleanup
