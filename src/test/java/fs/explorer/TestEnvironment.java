b7f23f95dbef711089fc602830731019e1617c62
Marat Habibullin <maratx387@gmail.com>
2017-08-08 13:25:28 +0300

    AsyncFsDataProviderTest is tested

--- src/test/java/fs/explorer/TestEnvironment.java
+++ src/test/java/fs/explorer/TestEnvironment.java
@@ -2,8 +2,13 @@ package fs.explorer;
 
 public class TestEnvironment {
     private static final String TEST_FTP = "testFtp";
+    private static final String TEST_ASYNC = "testAsync";
 
     public static boolean ftpTestsNeeded() {
         return "true".equalsIgnoreCase(System.getProperty(TEST_FTP));
+    }
+
+    public static boolean asyncTestsNeeded() {
+        return "true".equalsIgnoreCase(System.getProperty(TEST_ASYNC));
     }
 }
---
+++ src/test/java/fs/explorer/providers/dirtree/AsyncFsDataProviderTest.java
@@ -0,0 +1,58 @@
+package fs.explorer.providers.dirtree;
+
+import fs.explorer.TestEnvironment;
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
+public class AsyncFsDataProviderTest {
+    private FsDataProvider fsDataProvider;
+    private AsyncFsDataProvider asyncFsDataProvider;
+    private CyclicBarrier cyclicBarrier;
+    private AtomicInteger counter;
+
+    @Before
+    public void setUp() {
+        Assume.assumeTrue(TestEnvironment.asyncTestsNeeded());
+
+        fsDataProvider = mock(FsDataProvider.class);
+        doAnswer(invocationOnMock -> {
+            cyclicBarrier.await(10, TimeUnit.SECONDS);
+            try {
+                Thread.sleep(10000);
+            } catch(InterruptedException e) {
+                counter.incrementAndGet();
+            } finally {
+                cyclicBarrier.await(10, TimeUnit.SECONDS);
+            }
+            return null;
+        }).when(fsDataProvider).getNodesFor(any(), any(), any());
+
+        cyclicBarrier = new CyclicBarrier(4);
+        counter = new AtomicInteger(0);
+
+        asyncFsDataProvider = new AsyncFsDataProvider(fsDataProvider);
+    }
+
+    @Test
+    public void interruptsWithShutdownNow()
+            throws InterruptedException, TimeoutException, BrokenBarrierException {
+        asyncFsDataProvider.getNodesFor(null, null, null);
+        asyncFsDataProvider.getNodesFor(null, null, null);
+        asyncFsDataProvider.getNodesFor(null, null, null);
+        cyclicBarrier.await(10, TimeUnit.SECONDS);
+        Thread.sleep(2000);
+        asyncFsDataProvider.shutdownNow();
+        cyclicBarrier.await(10, TimeUnit.SECONDS);
+        assertEquals(3, counter.get());
+    }
+}
